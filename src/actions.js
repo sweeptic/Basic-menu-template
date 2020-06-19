ce1f8f1494648c2448f2eea6af858c50956e9b23
Attila <59293104+sweeptic@users.noreply.github.com>
2020-06-19 20:23:18 +0200

    basic setup for redux, setup empry store and reducer trough provider

---
+++ src/actions.js
@@ -0,0 +1,6 @@
+// addIngredientHandler
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+
+// removeIngredientHandler
+export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
+
--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
 
+const store = createStore(reducer);
 
 const app = (
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-  
 )
 
 
