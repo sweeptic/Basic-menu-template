e252a6da7aa968083152b8b049c1a2c3fbaf9d8c
Attila <59293104+sweeptic@users.noreply.github.com>
2020-06-28 18:33:05 +0200

    5. Executing Asynchronous Code

--- src/BurgerBuilder.js
+++ src/BurgerBuilder.js
@@ -31,13 +31,13 @@ class BurgerBuilder extends Component {
    }
 
    componentDidMount() {
-      // axios.get('https://react-my-burger-3a440.firebaseio.com//ingredients.json')
-      //    .then(response => {
-      //       this.setState({ ingredients: response.data });
-      //    })
-      //    .catch(error => {
-      //       this.setState({ error: true });
-      //    })
+      axios.get('https://react-my-burger-3a440.firebaseio.com//ingredients.json')
+         .then(response => {
+            this.setState({ ingredients: response.data });
+         })
+         .catch(error => {
+            this.setState({ error: true });
+         })
    }
 
    // addIngredientHandler = (type) => {
