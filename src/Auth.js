407c827c7db5f4314d2044b2ceb0cfaa3dd997b8
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-06 16:39:06 +0200

    4. Sharing the Validation Method

--- src/Auth.js
+++ src/Auth.js
@@ -7,6 +7,7 @@ import * as actions from './Redux_actions_index'
 import { connect } from 'react-redux';
 import Spinner from './Spinner'
 import { Redirect } from 'react-router-dom';
+import { checkValidity } from './utility'
 
 class Auth extends Component {
 
@@ -50,36 +51,7 @@ class Auth extends Component {
       }
    }
 
-   checkValidity(value, rules) {
-      let isValid = true;
-      if (!rules) {
-         return true;
-      }
 
-      if (rules.required) {
-         isValid = value.trim() !== '' && isValid;
-      }
-
-      if (rules.minLength) {
-         isValid = value.length >= rules.minLength && isValid
-      }
-
-      if (rules.maxLength) {
-         isValid = value.length <= rules.maxLength && isValid
-      }
-
-      if (rules.isEmail) {
-         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-         isValid = pattern.test(value) && isValid
-      }
-
-      if (rules.isNumeric) {
-         const pattern = /^\d+$/;
-         isValid = pattern.test(value) && isValid
-      }
-
-      return isValid;
-   }
 
    inputChangedHandler = (event, controlName) => {
       const updatedControls = {
@@ -87,7 +59,7 @@ class Auth extends Component {
          [controlName]: {
             ...this.state.controls[controlName],
             value: event.target.value,
-            valid: this.checkValidity(
+            valid: checkValidity(
                event.target.value,
                this.state.controls[controlName].validation),
             touched: true
--- src/ContactData.js
+++ src/ContactData.js
@@ -7,6 +7,7 @@ import Input from './Input';
 import { connect } from 'react-redux';
 import withErrorHandler from './withErrorHandler';
 import * as actions from './Redux_actions_index';
+import { checkValidity } from './utility'
 
 class ContactData extends Component {
    state = {
@@ -73,7 +74,8 @@ class ContactData extends Component {
             },
             value: '',
             validation: {
-               required: true
+               required: true,
+               isEmail: true
             },
             valid: false,
             touched: false
@@ -113,36 +115,6 @@ class ContactData extends Component {
       this.props.onOrderBurger(order, this.props.token);
    }
 
-   checkValidity(value, rules) {
-      let isValid = true;
-      if (!rules) {
-         return true;
-      }
-
-      if (rules.required) {
-         isValid = value.trim() !== '' && isValid;
-      }
-
-      if (rules.minLength) {
-         isValid = value.length >= rules.minLength && isValid
-      }
-
-      if (rules.maxLength) {
-         isValid = value.length <= rules.maxLength && isValid
-      }
-
-      if (rules.isEmail) {
-         const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-         isValid = pattern.test(value) && isValid
-      }
-
-      if (rules.isNumeric) {
-         const pattern = /^\d+$/;
-         isValid = pattern.test(value) && isValid
-      }
-
-      return isValid;
-   }
 
    inputChangedHandler = (event, inputIdentifier) => {
       const updatedOrderForm = {
@@ -153,7 +125,7 @@ class ContactData extends Component {
       };
 
       updatedFormElement.value = event.target.value;
-      updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+      updatedFormElement.valid = checkValidity(updatedFormElement.value, updatedFormElement.validation)
       updatedFormElement.touched = true;
       updatedOrderForm[inputIdentifier] = updatedFormElement;
 
